e78ac0d2778275b6b6cf27f4d084dc8db9031673
Bighomieyelly <33910041+Bighomieyelly@users.noreply.github.com>
2019-02-05 21:52:44 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,7 @@
+$("button").click(function() {
+    var message = $("input").val();
+     var message = $("input#1").val();
+      var message = $("input#2").val();
+       var message = $("input#3").val();
+$(".message").text(message);
+});
